ddec1451556d5d69fc8ca4017b2a39478770adb2
Anna-bel25 <86028959+Anna-bel25@users.noreply.github.com>
2024-05-09 14:25:29 -0500

    Create materia_nivel.js

---
+++ materia_nivel.js
@@ -0,0 +1,29 @@
+[
+  {
+    "materia_id": 1,
+    "nombre": "Lenguaje y Comunicación",
+    "imagen": "https://images.unsplash.com/photo-1621452773781-0f992fd1f5cb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmklQzMlQjFvcyUyMGNvbnZlcnNhbmRvfGVufDB8fDB8fHww",
+    "fecha": "21st may, 2023",
+    "autor": "admin",
+    "descripcion": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae cupiditate ducimus enim sed illo aut?",
+    "nivel_id": 1
+  },
+  {
+    "materia_id": 2,
+    "nombre": "Nombre de la segunda materia",
+    "imagen": "URL de la imagen",
+    "fecha": "Fecha de la segunda materia",
+    "autor": "Autor de la segunda materia",
+    "descripcion": "Descripción de la segunda materia",
+    "nivel_id": 1
+  },
+  {
+    "materia_id": 3,
+    "nombre": "Nombre de la tercera materia",
+    "imagen": "URL de la imagen",
+    "fecha": "Fecha de la tercera materia",
+    "autor": "Autor de la tercera materia",
+    "descripcion": "Descripción de la tercera materia",
+    "nivel_id": 1
+  }
+]
